d0ee0b4a059709be26106a8f91837eeaf0400aa8
ei17ringo <ei17ringo@gmail.com>
2016-02-05 21:24:48 +0800

    commit init

--- sampleTableView002/ViewController.swift
+++ sampleTableView002/ViewController.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
 
+    @IBOutlet weak var myTableView: UITableView!
+    
+    var selectedIndex = -1
+    //データを配列で用意する
+    var tea_list = ["ダージリン","アールグレイ","アッサム","オレンジペコ"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,46 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // 行数
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tea_list.count
+    }
+    
+    // 表示するセルの中身
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell(style: .Default, reuseIdentifier: "myCell")
+        //cell.textLabel!.text = "\(indexPath.row)行目"
+        
+        //文字色を茶色にする
+        cell.textLabel?.textColor = UIColor.brownColor()
+        //矢印を右側につける
+        cell.accessoryType = .DisclosureIndicator
+        
+        cell.textLabel!.text = "\(tea_list[indexPath.row])"
+        return cell
+        
+    }
+    
+    // 選択された時に行う処理
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("\(indexPath.row)行目を選択")
+        selectedIndex = indexPath.row
+    performSegueWithIdentifier("showSecondView",sender: nil)
+    }
 
-
+    // Segueで画面遷移する時
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        var secondVC = segue.destinationViewController as! secondViewController
+        
+        secondVC.scSelectedIndex = selectedIndex
+        
+//            if (segue.identifier == "showSecondView") {
+//                let secondVC: secondViewController = (segue.destinationViewController as? secondViewController)!
+//                
+//                // SecondViewControllerのtextに選択した文字列を設定する
+//                // secondVC.text = selectedText
+//            }
+    }
 }
 
